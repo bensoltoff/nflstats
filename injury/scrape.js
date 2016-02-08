21cd691fdc9042a2170b433e652119ff8ef03253
bensoltoff <ben@bensoltoff.com>
2016-02-08 11:12:08 -0500

    Delete old javascript files

--- injury/scrape.js
+++
@@ -1,9 +0,0 @@
-var page = require('webpage').create();
-page.open('http://www.nfl.com/injuries?week=1', function () {
-                   page.evaluate(function(){
-
-                   });
-                   fs.write('1.html', page.content, 'w');
-
-                   phantom.exit();
-                   });
